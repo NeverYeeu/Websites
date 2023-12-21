9fc724ddb9407c55338cbd61d6fb0ab0ea249edf
Ng.V.Hoàng <134377540+NeverYeeu@users.noreply.github.com>
2023-12-21 14:39:18 +0700

    upcode

---
+++ app.js
@@ -0,0 +1,65 @@
+const api = [
+	{
+		"website" : [
+			{
+				id: 0,
+				webName: 'ReadicCom',
+				webImg: 'img/readicCom.jpg',
+				webHref: 'https://neveryeeu.github.io/WebComics/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'vn'
+			},
+			{
+				id: 1,
+				webName: 'Nettruyen',
+				webImg: 'img/nettruyen.jpg',
+				webHref: 'https://www.nettruyenus.com/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'vn'
+			},
+			{
+				id: 2,
+				webName: 'Truyenqq',
+				webImg: 'img/truyenqqq.jpg',
+				webHref: 'https://truyenqqvn.com/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'vn'
+			},
+			{
+				id: 3,
+				webName: 'Blog Truyen',
+				webImg: 'img/blogtruyen.jpg',
+				webHref: 'https://blogtruyen.vn/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'vn'
+			},
+			{
+				id: 4,
+				webName: 'Mangakatana',
+				webImg: 'img/mangakatana.jpg',
+				webHref: 'https://mangakatana.com/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'en'
+			},
+			{
+				id: 5,
+				webName: 'Mangakakalot',
+				webImg: 'img/mangakakalot.jpg',
+				webHref: 'https://ww7.mangakakalot.tv/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'en'
+			},
+			{
+				id: 6,
+				webName: 'Manga Park',
+				webImg: 'img/mangapark.jpg',
+				webHref: 'https://mangapark.net/',
+				webDescription: 'Mô tả về trang Web',
+				webLang: 'en'
+			},
+
+		]
+
+	}
+]
+export {api}
---
+++ index.js
@@ -0,0 +1,51 @@
+import {api} from "./app.js"
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+let getWebsite = api[0].website;
+let countryBtns = $$('.nav-country > li');
+// Chuong trinh se chay khi moi duyet web
+function webStart(){
+	let getIndex = localStorage.getItem('indexLang');
+	$('.nav-country > li.choose-country').classList.remove('choose-country');
+	countryBtns[getIndex].classList.add('choose-country');
+	let getName = countryBtns[getIndex].innerText.toLowerCase();
+	getArrayLanguage(getName)
+} webStart()
+// Xu ly khi ta click vao thanh nav
+function handleCountryBtn(){
+	countryBtns.forEach((btn, index) => {
+		btn.addEventListener('click', () => {
+			$('.nav-country > li.choose-country').classList.remove('choose-country');
+			btn.classList.add('choose-country');
+			let nameLanguage = btn.innerText.toLowerCase();
+			getArrayLanguage(nameLanguage);
+			localStorage.setItem('indexLang', index);
+		})
+	})
+}; handleCountryBtn();
+function getArrayLanguage(name) {
+	let arrayLanguage = getWebsite.filter((lang) => {
+		return lang.webLang.includes(name);
+	});
+	handleWebsite(arrayLanguage)
+};
+function handleWebsite(arr){
+	let boxInfor = $('#box-infor');
+	boxInfor.innerHTML = arr.map(renderWebsite).join('');
+	function renderWebsite(web){
+		let {webName, webImg, webHref, webDescription} = web;
+		return (`
+		<div class="infor-wrap">
+					<img src=${webImg} alt="" class="wrap_img">
+					<div class="wrap-head">
+						<span>${webName}</span>
+						<div class="list-genre">${webDescription}</div>
+						<a href=${webHref} class="read-btn href">Đọc truyện</a>
+					</div>
+					<i class="fa-solid fa-bars genre-icon"></i>
+				</div>
+		`)
+	};
+
+}	
